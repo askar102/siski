b883bfe5bba0bb88d80be5dbeb18f7b5b0073597
askar102 <sergey.bolotny@icloud.com>
2026-06-24 09:59:34 +0500

    Created: Source folder

---
+++ src/lexer.cpp
@@ -0,0 +1,275 @@
+#include "lexer.h"
+
+void Lexer::Tokenize(const std::string& content) {
+    for (size_t i = 0; i <= content.size()-1; ++i) {
+        if (std::isspace(content[i])) {
+            continue;
+        }
+
+        if (std::isdigit(content[i])) {
+            CheckDigit(content, i);
+        }
+
+
+        // check exp. operators ->  keywords 
+        if (std::isalpha(content[i])) {
+            CheckAlpha(content, i);
+        }
+        
+
+        CheckAloneOperators(content, i);
+        continue;
+    }
+
+    _tokens.push_back({TokenType::C_EOF, ""});
+    LOG("EOF REACHED\n");
+}
+
+const std::vector<std::pair<TokenType, std::string>>& Lexer::GetTokens() const {
+    return _tokens;
+}
+
+void Lexer::CheckDigit(const std::string& content, size_t& i) {
+    if (std::isdigit(content[i])) {
+        std::string value;
+
+        while (i < content.size() && (std::isdigit(content[i]))) {
+            value += content[i];
+            ++i;
+        }
+
+        _tokens.push_back({TokenType::INT_LITERAL, value});
+
+        LOG("DIGIT: %s\n", value.c_str());
+    }
+}
+
+void Lexer::CheckAlpha(const std::string& content, size_t& i) {
+    if (std::isalpha(content[i])) {
+        std::string value;
+
+        while (i < content.size() && (std::isalnum(content[i]) || content[i] == '_')) {
+            value += content[i];
+            ++i;
+        }
+
+        LOG("FOUND WORD: %s\n", value.c_str());
+        
+        CheckKeywords(value);
+    }
+}
+
+void Lexer::CheckAloneOperators(const std::string& content, size_t& i) {
+    switch (content[i]) {
+        case '+':  {
+            _tokens.push_back({TokenType::PLUS, "+"});
+            LOG("PLUS: +\n");
+            break;
+        }
+
+        case '-': {
+            if (i < content.size() && content[i+1] == '>') {
+                _tokens.push_back({TokenType::FUNC_RETURN_TYPE_OP, "->"});
+                LOG("FUNC_RETURN_TYPE_OP: >=\n");
+                ++i;
+                break;
+            }
+
+            _tokens.push_back({TokenType::MINUS, "-"});
+            LOG("MINUS: -\n");
+            break;
+        }
+
+        case '*': {
+            _tokens.push_back({TokenType::STAR, "*"});
+            LOG("STAR: *\n");
+            break;
+        }
+
+        case '/': {
+            _tokens.push_back({TokenType::SLASH, "/"});
+            LOG("SLASH: /\n");
+            break;
+        }
+
+        case '(': {
+            _tokens.push_back({TokenType::LPAREN, "("});
+            LOG("LPAREN: (\n");
+            break;
+        }
+
+        case ')': {
+            _tokens.push_back({TokenType::RPAREN, ")"});
+            LOG("RPAREN: )\n");
+            break;
+        }
+
+        case '{': {
+            _tokens.push_back({TokenType::LBRACE, "{"});
+            LOG("LBRACE: {\n");
+            break;
+        }
+
+        case '}': {
+            _tokens.push_back({TokenType::RBRACE, "}"});
+            LOG("RBRACE: }\n");
+            break;
+        }
+
+        case '[': {
+            _tokens.push_back({TokenType::LBRACKET, "["});
+            LOG("LBRACKET: [\n");
+            break;
+        }
+
+        case ']': {
+            _tokens.push_back({TokenType::RBRACKET, "]"});
+            LOG("RBRACKET: ]\n");
+            break;
+        }
+
+        case ';': {
+            _tokens.push_back({TokenType::SEMICOLON, ";"});
+            LOG("SEMICOLON: ;\n");
+            break;
+        }
+
+        case ':': {
+            _tokens.push_back({TokenType::COLON, ":"});
+            LOG("COLON: :\n");
+            break;
+        }
+
+        case ',': {
+            _tokens.push_back({TokenType::COMMA, ","});
+            LOG("COMMA: ,\n");
+            break;
+        }
+
+        case '&': {
+            _tokens.push_back({TokenType::AMPERSAND, "&"});
+            LOG("AMPERSAND: &\n");
+            break;
+        }
+
+        case '=': {
+            if (i < content.size() && content[i+1] == '=') {
+                _tokens.push_back({TokenType::EQUAL, "=="});
+                LOG("EQUAL: ==\n");
+                ++i;
+                break;
+            }
+
+            _tokens.push_back({TokenType::ASSIGN, "="});
+            LOG("ASSIGN: =\n");
+            break;
+        }
+
+        case '<': {
+            // todo =<
+            if (i < content.size() && content[i+1] == '=') {
+                _tokens.push_back({TokenType::EQUAL_AND_LESS, "<="});
+                LOG("QUAL_AND_LESS: <=\n");
+                ++i;
+                break;
+            }
+
+            _tokens.push_back({TokenType::LESS, "<"});
+            LOG("LESS: <\n");
+            break;
+        }
+
+        case '>': {
+            if (i < content.size() && content[i+1] == '=') {
+                _tokens.push_back({TokenType::EQUAL_AND_GREATER, ">="});
+                LOG("QUAL_AND_GREATER: >=\n");
+                ++i;
+                break;
+            }
+
+            _tokens.push_back({TokenType::GREATER, ">"});
+            LOG("GREATER: >\n");
+            break;
+        }
+
+        default: {
+            LOG("CANT FIND OPERATOR SUKA: %c\n", content[i]);
+            break;
+        }
+    }
+}
+
+void Lexer::CheckKeywords(const std::string& keyword) {
+    if (!keyword.empty()) {
+        if (keyword == "if") {
+            _tokens.push_back({TokenType::IF, "if"});
+            LOG("IF: if\n");
+            return;
+        }
+        else if (keyword == "else") {
+            _tokens.push_back({TokenType::ELSE, "else"});
+            LOG("ELSE: else\n");
+            return;
+        }
+        else if (keyword == "goto") {
+            _tokens.push_back({TokenType::GOTO, "goto"});
+            LOG("GOTO: goto\n");
+            return;
+        }
+        else if (keyword == "label") {
+            _tokens.push_back({TokenType::LABEL, "label"});
+            LOG("LABEL: label\n");
+            return;
+        }
+
+        else if (keyword == "int") {
+            _tokens.push_back({TokenType::INT, "int"});
+            LOG("INT: int\n");
+            return;
+        }
+        else if (keyword == "void") {
+            _tokens.push_back({TokenType::VOID, "void"});
+            LOG("VOID: void\n");
+            return;
+        }
+
+        else if (keyword == "func") {
+            _tokens.push_back({TokenType::FUNC, "func"});
+            LOG("FUNC: func\n");
+            return;
+        }
+
+        else if (keyword == "return") {
+            _tokens.push_back({TokenType::RETURN, "return"});
+            LOG("RETURN: return\n");
+            return;
+        }
+
+        else if (keyword == "exC") {
+            _tokens.push_back({TokenType::EXTERN_C, "exC"});
+            LOG("EXTERN_C: exC\n");
+            return;
+        }
+
+        else if (keyword == "export") {
+            _tokens.push_back({TokenType::EXPORT_C, "export"});
+            LOG("EXPORT_C: export\n");
+            return;
+        }
+
+        else if (keyword == "as") {
+            _tokens.push_back({TokenType::AS_C, "as"});
+            LOG("AS_C: as\n");
+            return;
+        }
+
+        // IDENT CHECK
+        else {
+            _tokens.push_back({TokenType::IDENTIFIER, keyword});
+            LOG("INDENT: %s\n", keyword.c_str());
+            return;
+        }
+    }
+
+    return;
+}
---
+++ src/lexer.h
@@ -0,0 +1,49 @@
+#pragma once
+
+#include <string>
+#include <fstream>
+#include <vector>
+
+#include "log.h"
+
+enum TokenType {
+    // keywords
+    IF, ELSE, 
+    GOTO, LABEL,
+    INT, VOID,
+    FUNC, RETURN,
+
+    // another
+    IDENTIFIER, INT_LITERAL, FUNC_RETURN_TYPE_OP, 
+    EXTERN_C, EXPORT_C, AS_C,
+
+    // expression operators
+    ASSIGN, PLUS, MINUS, STAR, SLASH, AMPERSAND,
+    // condition operators
+    EQUAL, LESS, GREATER, EQUAL_AND_LESS, EQUAL_AND_GREATER,
+
+
+    LPAREN, RPAREN, LBRACE, RBRACE, LBRACKET, RBRACKET, SEMICOLON, COMMA, COLON,
+
+    C_EOF, C_ERROR,
+};
+
+
+class Lexer {
+public:
+    Lexer() = default;
+
+    void Tokenize(const std::string& content);
+    const std::vector<std::pair<TokenType, std::string>>& GetTokens() const;
+
+private:
+    std::vector<std::pair<TokenType, std::string>> _tokens;
+
+    void CheckDigit(const std::string& content, size_t& i);
+
+    void CheckAlpha(const std::string& content, size_t& i);
+
+    void CheckAloneOperators(const std::string& content, size_t& i);
+
+    void CheckKeywords(const std::string& keyword);
+};

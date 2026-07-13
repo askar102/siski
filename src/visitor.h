cb2a98d3a2a21162fc408964ba3db72f31454149
askar102 <sergey.bolotny@icloud.com>
2026-07-13 10:51:35 +0500

    added visitor

---
+++ src/visitor.h
@@ -0,0 +1,41 @@
+#pragma once
+
+// forward-декларации всех конкретных нод
+class NumberNode;
+class BinaryExpression;
+class UnaryNode;
+class VariableRefNode;
+class VariableDeclNode;
+class VariableAssignNode;
+class ReturnStatement;
+class IfStatementNode;
+class BlockNode;
+class FunctionCallNode;
+class GotoStatement;
+class LabelStatement;
+class FuncDeclNode;
+class ArgNode;
+class TypeNode;
+class RootNode;
+
+class Visitor {
+public:
+    virtual ~Visitor() = default;
+
+    virtual void visit(NumberNode&) = 0;
+    virtual void visit(BinaryExpression&) = 0;
+    virtual void visit(UnaryNode&) = 0;
+    virtual void visit(VariableRefNode&) = 0;
+    virtual void visit(VariableDeclNode&) = 0;
+    virtual void visit(VariableAssignNode&) = 0;
+    virtual void visit(ReturnStatement&) = 0;
+    virtual void visit(IfStatementNode&) = 0;
+    virtual void visit(BlockNode&) = 0;
+    virtual void visit(FunctionCallNode&) = 0;
+    virtual void visit(GotoStatement&) = 0;
+    virtual void visit(LabelStatement&) = 0;
+    virtual void visit(FuncDeclNode&) = 0;
+    virtual void visit(ArgNode&) = 0;
+    virtual void visit(TypeNode&) = 0;
+    virtual void visit(RootNode&) = 0;
+};
